b05119509aaab01e9ca0c69b5d4f10634408a9a7
Rafal Staron <s12570@pjwstk.edu.pl>
2016-12-05 00:55:00 +0100

    Base of ImportWorker class

--- PLE/PLE/ImWorker.cs
+++ PLE/PLE/ImWorker.cs
@@ -1,4 +1,7 @@
-﻿namespace PLE
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace PLE
 {
     internal class ImWorker
     {
@@ -11,6 +14,16 @@
             this.path = path;
             this.lineStart = v;
             this.lineEnd = lineCount;
+        }
+        public List<string> readLines()
+        {
+            int i = 0;
+            while (i < lineStart)
+            {
+                StreamReader sr = new StreamReader(path);
+
+            }
+            return null;
         }
     }
 }
