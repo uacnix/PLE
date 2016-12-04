cc8d60c1cc89079f681d0f237dc5546c2c6c9a63
Rafal Staron <s12570@pjwstk.edu.pl>
2016-12-04 21:08:24 +0100

    First upload

---
+++ WpfApplication1/WpfApplication1/ImWorker.cs
@@ -0,0 +1,16 @@
+﻿namespace WpfApplication1
+{
+    internal class ImWorker
+    {
+        private int lineEnd;
+        private string path;
+        private int lineStart;
+
+        public ImWorker(string path, int v, int lineCount)
+        {
+            this.path = path;
+            this.lineStart = v;
+            this.lineEnd = lineCount;
+        }
+    }
+}
---
+++ WpfApplication1/WpfApplication1/Importer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfApplication1
+{
+    internal class Importer
+    {
+        private string path;
+        private int lineCount;
+        private int threadCount;
+        public Importer(string path,int threads)
+        {
+            this.path = path;
+            threadCount = threads;
+        }
+
+        public void Import()
+        {
+            //TODO: implement thread count selector.
+            lineCount = getLineCount();
+            int linePool = lineCount / threadCount;
+            List<ImWorker> workers = new List<ImWorker>();
+            for(int i = 0; i < threadCount; i++)
+            {
+                ImWorker w;
+                if (i!=threadCount-1)
+                    w = new ImWorker(path, linePool * i, linePool * (i + 1));
+                else
+                {
+                    w = new ImWorker(path, linePool * i, lineCount);
+                }
+                workers.Add(w);
+            }
+        }
+
+        private int getLineCount()
+        {
+            return null;
+        }
+    }
+}
